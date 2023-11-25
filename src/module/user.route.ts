fa5108842862faf1cfab7e39ac08ec55f6266dd2
nrb04 <nredoyan@gmail.com>
2023-11-25 18:23:23 +0600

    post data sucessfull

--- src/module/user.route.ts
+++ src/module/user.route.ts
@@ -2,6 +2,19 @@ import express from "express";
 import { UserController } from "./user.controller";
 
 const router = express.Router();
+
+// Create a new user
 router.post("/createuser", UserController.createUser);
+
+// Retrieve all users
 router.get("/", UserController.getAllUsers);
+
+// Retrieve a specific user by ID
+router.get("/:userId", UserController.getUserById);
+
+// Update user information
+router.put("/:userId", UserController.updateUser);
+
+router.delete("/:userId", UserController.deleteUser);
+
 export const userroutes = router;
